bd41f42516ff639a65df927b3ed18bcac49d6d5b
Katy Kane <katytheartist@gmail.com>
2024-04-08 17:56:15 -0500

    push 1

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default class Form extends React.Component {
+  render() {
+    return (
+  <>
+    <form id='todoForm' 
+      onSubmit={this.props.onTodoFormSubmit}>
+      <input 
+        value={this.props.todoNameInput} 
+        onChange= {this.props.onTodoChange} 
+        type='text' 
+        placeholder='Type todo'>
+      </input>
+      <input 
+        type='submit'>
+      </input>
+    </form>
+    <button 
+      onClick={this.props.toggleDisplayCompleteds}>
+      {this.props.displayCompleteds ? 'Hide': 'Show'} Completed
+    </button>
+  </>
+    )
+  }
+}
